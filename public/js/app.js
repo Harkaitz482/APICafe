06106a8830700fb0885d1fff21a5106e0cdb7abe
Nerv00x <aridanecabreraramirez@gmail.com>
2023-12-18 14:59:55 +0000

    Cambios user

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             enviarCambios();
-            cambiosUser();
+            
         });
 
         horasTotalesElement.textContent = horasTotales;
